a3f913baf2bda7266bf75709a77d08317d39934f
SukhoparovAS <77128030+SukhoparovAS@users.noreply.github.com>
2021-03-04 15:23:48 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	$('.burger').click(function(event) {
+		$('.burger , .menu').toggleClass('active');
+	});
+	$('.slider').slick({
+		arrows:false,
+		dots:true,
+		adaptiveHeight:false,
+		slidesToShow:1,
+		slidesToScroll:1,
+		speed:1000,
+		easing:'linear',
+		infinite:true,
+		initialSlide:1,
+		autoplay:true,
+		autoplaySpeed:5000,
+		pausOnFocus:true,
+		pauseOnHover:true,
+		pauseOnDotsHover:true,
+		draggable:true,
+		swipe:true,
+		touchThreshold:7,
+		touchMove:true,
+		waitForAnimate:false,
+		centerMode:false,
+		variableWidth:false,
+		rows:1,
+		fade:false,
+	});
+	console.log('dsa');
+});
